c5c973242fcb9bc49067d668457f06e3a6059c95
Karl Erik Nelson <karlerik.nelson@tptlive.ee>
2024-11-29 11:40:39 +0200

    seeder

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,13 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
 
-    protected $fillable = ['title', 'body'];
+    protected $fillable = ['title', 'body', 'image'];
 
     // public function getSnippetAttribute(){
     //     return explode("\n\n", $this->body)[0];
@@ -25,7 +27,29 @@ class Post extends Model
 
     public function displayBody(): Attribute {
         return Attribute::get(function (){
-            return nl2br($this->body);
+            return $this->body;
+        });
+        return Storage::disk('public')->url($this->image);
+    }
+
+    public function iimage(): Attribute {
+        return Attribute::set(function ($image){
+            if($image instanceof UploadedFile){
+                return $image->store('',['disk' => 'public']);
+            }
+            return $image;
+        });
+    }
+
+    /**
+     * The "booted" method of the model.
+     */
+    protected static function booted(): void
+    {
+        static::deleted(function (Post $post) {
+            if($post->image){
+                Storage::disk('public')->delete($post->image);
+            }
         });
     }
 }
